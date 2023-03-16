24653b5fc93139229a148db2c7ff1c3e6e962fe1
Nicolas_Gomez_Barbosa <n.gomezb2@uniandes.edu.co>
2023-03-16 02:05:18 -0500

    Se agrega funcionalidad del autorizador para generar un Token al Inicio de sesión

---
+++ autorizador-generador/app.py
@@ -0,0 +1,19 @@
+from flask import Flask 
+from flask_cors import CORS
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from vista import VistaToken
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dbapp.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'frase-secreta'
+
+app_context = app.app_context()
+app_context.push()
+
+cors = CORS(app)
+
+api = Api(app)
+api.add_resource(VistaToken, '/token')
+jwt = JWTManager(app)
