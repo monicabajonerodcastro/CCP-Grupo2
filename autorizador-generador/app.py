6ab072defb91cd88504dac175039dac21555f05f
Juan Sebastian Becerra Bautista <j.becerrab@uniandes.edu.co>
2023-03-17 16:26:28 -0500

    feat: propragar excepcion del token

--- autorizador-generador/app.py
+++ autorizador-generador/app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
@@ -8,6 +8,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dbapp.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'frase-secreta'
+app.config['PROPAGATE_EXCEPTIONS'] = True
 
 app_context = app.app_context()
 app_context.push()
